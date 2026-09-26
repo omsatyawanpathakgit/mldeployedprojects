3315183556350b429c557918a6bab586131485e1
omsatyawanpathakgit <omsatyawanpathakgit@gmail.com>
2026-09-26 11:24:34 +0530

    Prepare customer churn project for Vercel

--- customerchurnprediction/api/app.py
+++ customerchurnprediction/api/app.py
@@ -3,14 +3,12 @@ import pandas as pd
 import joblib
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(
     __name__,
     template_folder=os.path.join(BASE_DIR, "frontend")
 )
-
 
 MODEL_PATH = os.path.join(
     BASE_DIR,
